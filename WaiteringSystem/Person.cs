ef924b4d2746db32ec7adce7ed4d40bd57035f8e
Tendai <youremail@example.com>
2024-08-02 15:38:41 +0200

    added the relevant methods

---
+++ WaiteringSystem/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaiteringSystem
+{
+    public abstract class Person
+    {   //region for data members
+        #region Data Members
+        private string id;
+        private string name;
+        private int phone;
+        #endregion
+
+        //region for properties
+        #region Property Methods
+        public string ID{
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Name{ 
+            get { return name; } 
+            set { name = value; } 
+        }
+
+        public string Name 
+            { 
+            get { return name; }
+            set { id = value; }
+
+        }
+
+        public int Phone { 
+            get { return phone; }
+            set { phone = value; }
+        }
+        #endregion
+
+        //region for constructors
+    }
+}
