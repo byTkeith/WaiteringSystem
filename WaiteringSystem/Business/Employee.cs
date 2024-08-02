beff9cccb4128e35ce0257453791b1afbc637822
Tendai <youremail@example.com>
2024-08-02 15:58:43 +0200

    added waiter cases

--- WaiteringSystem/Business/Employee.cs
+++ WaiteringSystem/Business/Employee.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace WaiteringSystem.Business
 {
-    internal class Employee
+    public class Employee:Person
     {
+        public int EmployeeNumber{
+            get; set;
+        }
+        public Role Role { get; set; }
+
+        // the constructors
+        public Employee(Role roleValue) {
+            switch (roleValue.RoleVal) {
+                case Role.RoleType.NoRole:
+                    Role = new Role();
+                    break;
+                case Role.RoleType.HeadWaiter:
+                    Role = new HeadWaiter();
+                    break;
+                case Role.RoleType.Waiter:
+                    Role = new Waiter;
+                    break;
+                case Role.RoleType.Runner:
+                    Role = new Runner();
+                    break;
+            }
+        }
     }
 }
